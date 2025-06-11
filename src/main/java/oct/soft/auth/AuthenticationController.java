fdd7cfd7565474fa310cf89185a67b9b0169c564
osantau <octavian.santau.os@gmail.com>
2025-06-11 19:06:59 +0300

    Add Swagger annotantions dependency.

--- src/main/java/oct/soft/auth/AuthenticationController.java
+++ src/main/java/oct/soft/auth/AuthenticationController.java
@@ -4,8 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.tags.Tag;

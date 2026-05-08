d9168ca1c12a15504c3e8537b8cd751898865cb9
osantau <octavian.santau@plastor.ro>
2026-05-08 13:48:34 +0300

    Implement filters and security filter chain.

---
+++ src/main/java/oct/soft/security/JwtFilter.java
@@ -0,0 +1,44 @@
+package oct.soft.security;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+	private final JwtService jwtService;
+	@Override
+	protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+			@NonNull FilterChain filterChain) throws ServletException, IOException {
+		if(request.getServletPath().contains("/api/v1/auth"))
+		{
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
+		final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+		final String jwt;
+		final String userEmail;
+		if(authHeader==null || !authHeader.startsWith("Bearer "))
+		{
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
+		jwt = authHeader.substring(7);		
+		userEmail = jwtService.extractUsername(jwt);
+		
+	}
+
+}
---
+++ src/main/java/oct/soft/security/JwtService.java
@@ -0,0 +1,12 @@
+package oct.soft.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtService {
+
+	public String extractUsername(String jwt) {
+		return null;
+	}
+
+}

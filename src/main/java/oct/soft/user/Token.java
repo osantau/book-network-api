93e933b7d7e75838893676c7272c7699881212b2
osantau <octavian.santau@plastor.ro>
2026-05-08 12:57:11 +0300

    Add Token entity and repository.

---
+++ src/main/java/oct/soft/user/Token.java
@@ -0,0 +1,32 @@
+package oct.soft.user;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@Entity
+public class Token {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+    @ManyToOne
+    @JoinColumn(name="userId", nullable = false)
+    private User user;
+}
---
+++ src/main/java/oct/soft/user/TokenRepository.java
@@ -0,0 +1,14 @@
+package oct.soft.user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Long> {
+
+	Optional<Token> findByToken(String token);
+	
+}

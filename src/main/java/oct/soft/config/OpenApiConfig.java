3e9281acf859f3ea00e06a11bd9925be2ed13a2a
osantau <octavian.santau@plastor.ro>
2026-05-13 12:02:04 +0300

    Add swgger security specs plus additional information.

---
+++ src/main/java/oct/soft/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package oct.soft.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(info = @Info(contact = @Contact(name = "Octavian Santau", email = "sanyoct@yahoo.com"), description = "OpenAi documentation for Spring Security", title = "OpenApi specification = Octavian", version = "1.0", license = @License(name = "License Name", url = "https://some-url.com")
+
+		, termsOfService = "Terms Of Service"), servers = {
+				@Server(description = "Local ENV", url = "http://localhost:8088/api/v1"),
+				@Server(description = "PROD ENV", url = "http://octaviansantau/course") }, security = {
+						@SecurityRequirement(name = "bearerAuth") })
+@SecurityScheme(name = "bearerAuth", description = "JWT auth description", scheme = "bearer", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", in = SecuritySchemeIn.HEADER)
+public class OpenApiConfig {
+
+}
